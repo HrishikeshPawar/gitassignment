a0b4d4b323426d60921ce152a4c6fa528422ce65
HrishikeshPawar <128132718+HrishikeshPawar@users.noreply.github.com>
2023-04-02 13:35:35 +0530

    Java Code Uploaded

---
+++ Question1.java
@@ -0,0 +1,17 @@
+package Assignment2;
+//Write a program to calculate the sum of first 10 natural number.
+
+public class Question1 {
+
+	public static void main(String[] args) {
+	 int i,num=10, sum=0;
+		for(i=1;i<=num;++i)
+	  {
+		  sum= sum+i;
+	  }
+		  
+			System.out.println("Sum of First10 Natural NO:"+sum);
+	
+		
+	}
+}
